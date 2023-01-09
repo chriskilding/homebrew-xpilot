e0b81924c63b66f1ac95c73a32cea7202f3e7daa
Chris Kilding <590569+chriskilding@users.noreply.github.com>
2023-01-09 19:52:26 +0000

    Fix typo

--- Casks/xpilot.rb
+++ Casks/xpilot.rb
@@ -1,7 +1,7 @@
 cask "xpilot" do
 
   version "2.0.0-beta.42"
-  sha256 "5380a6c3589a1e00f8a42c97cf7cbd6db389d2a337edc0cbbd3a700e9347e483",
+  sha256 "5380a6c3589a1e00f8a42c97cf7cbd6db389d2a337edc0cbbd3a700e9347e483"
 
   url "https://github.com/xpilot-project/xpilot/releases/download/v#{version}/xPilot-#{version}-MacOS.dmg"
   
